45eb7d3df6877f80efb9e8b69d0c2669b9afa45d
Ryan Day <rddays@gmail.com>
2019-09-11 12:54:40 -0600

    Add state space equations

---
+++ hw1/kalman_filter.py
@@ -0,0 +1,16 @@
+import numpy as np
+from control import StateSpace, matlab
+
+from hw1.parameters import *
+
+
+m = VEHICLE_MASS
+b = LINEAR_DRAG_COEFFICIENT
+
+A = np.array([[0, 1], [0, -b/m]])
+B = np.array([[0], [1/m]])
+C = np.array([[1, 0]])
+D = np.array([[0]])
+state_space = StateSpace(A, B, C, D)
+discrete_time = matlab.c2d(state_space, SAMPLE_PERIOD)
+
