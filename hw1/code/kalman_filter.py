a6545389c469a802305c9c3f8bef5e4680c22c69
Ryan Day <rddays@gmail.com>
2019-09-13 13:22:04 -0600

    Format file to PEP8

--- hw1/code/kalman_filter.py
+++ hw1/code/kalman_filter.py
@@ -16,22 +16,26 @@ C = np.array([[1, 0]])
 Ctranspose = np.transpose(C)
 D = np.array([[0]])
 R = np.array([[MEASUREMENT_NOISE_COVARIANCE]])
-Q = np.array([[PROCESS_NOISE_COVARIANCE_POSITION],[PROCESS_NOISE_COVARIANCE_VELOCITY]])
+Q = np.array([[PROCESS_NOISE_COVARIANCE_POSITION],
+              [PROCESS_NOISE_COVARIANCE_VELOCITY]])
 state_space = StateSpace(A, B, C, D)
 discrete_time = matlab.c2d(state_space, SAMPLE_PERIOD)
+
 
 def kalman_filter(mean_prev, variance_prev, ut, zt, A, B, C, Atranspose, Ctranspose, R, Q):
     mean_belief = A*mean_prev + B*ut
     variance_belief = A*variance_prev*Atranspose + R
-    
+
     Kt = find_Kt(variance_belief, C, Ctranspose, Q)
-    
-    corrected_mean_belief = mean_belief + matmul(Kt, (zt - matmul(C, mean_belief)))
-    
+
+    corrected_mean_belief = mean_belief + \
+        matmul(Kt, (zt - matmul(C, mean_belief)))
+
     first = matmul(Kt, C)
     identity_matrix = identity(first.size())
-    corrected_var_belief = matmul((identity_matrix-first), variance_belief)
+    corrected_var_belief = matmul((identity_matrix - first), variance_belief)
     return corrected_mean_belief, corrected_var_belief
+
 
 def find_Kt(variance_belief, C, Ctranspose, Q):
     first = matmul(variance_belief, Ctranspose)
