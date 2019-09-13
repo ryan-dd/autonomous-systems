3a335541166a27623a64e2233e1350d362deffa1
Ryan Day <rddays@gmail.com>
2019-09-13 14:09:27 -0600

    Add default parameter to plotting

--- tools/plot_helper.py
+++ tools/plot_helper.py
@@ -4,7 +4,7 @@ import scipy.stats as stats
 from math import sqrt
 
 
-def create_plotting_parameters_for_normal_distribution(mean, variance):
+def create_plotting_parameters_for_normal_distribution(mean, variance, num_std_dev_to_plot=3):
     """Create parameters to easily plot a normal distribution
 
     Arguments:
@@ -13,9 +13,10 @@ def create_plotting_parameters_for_normal_distribution(mean, variance):
 
     """
     sigma = sqrt(variance)
-    # I set the range of x to be plus or minus 3 sigma since that has
+    # I set the default range of x to be plus or minus 3 sigma since that has
     # the important visual part of the normal distribution
-    x = np.linspace(mean - 3*sigma, mean + 3*sigma, 100)
+    x = np.linspace(mean - num_std_dev_to_plot*sigma,
+                    mean + num_std_dev_to_plot*sigma, 100)
     y = stats.norm.pdf(x, mean, sigma)
     return x, y
 
