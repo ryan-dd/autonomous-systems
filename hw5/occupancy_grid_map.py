b2975ea06444b270a318e6fb90fffe762f58f5e9
Ryan Day <rddays@gmail.com>
2019-10-23 12:16:24 -0600

    Fix bugs, reorganize code, add comments

--- hw5/occupancy_grid_map.py
+++ hw5/occupancy_grid_map.py
@@ -21,33 +21,34 @@ grid_map = []
 for i in range(100):
     grid_map.append([])
     for j in range(100):
-        # Insert log odds related to p = 0.5
+        # Insert log odds related to p = 0.5, which correlates to no information about whether
+        # the cell is occupied
         grid_map[i].append(L0)
 
-for time_step in range(len(ranges)):
-    for i in range(100):
-        for j in range(100):
-            grid_cell_probability = grid_map[i][j]
-            cell_occupied = check_grid_occupancy(i, j)
-            if cell_occupied:
-                grid_map[i][j] = grid_cell_probability + inverse_range_sensor_model([i, j], states, ranges, bearings, pointing_angles) - L0
-
-
-def check_grid_occupancy(i, j):
-    return True
-
-def inverse_range_sensor_model(cell, xt, z_ranges, z_bearings, z_pointing_angles):
-    xi = cell[0] - OFFSET_DISTANCE
-    yi = cell[1] - OFFSET_DISTANCE
-    x = xt[0]
-    y = xt[1]
-    theta = xt[2]
+# Define sensor model
+def inverse_range_sensor_model(cell, state_t, z_ranges, z_bearings, z_pointing_angles):
+    xi = cell[0] + OFFSET_DISTANCE
+    yi = cell[1] + OFFSET_DISTANCE
+    x = state_t[0]
+    y = state_t[1]
+    theta = state_t[2]
     r = np.sqrt((xi-x)**2 + (yi-y)**2)
     phi = np.arctan2(yi - y, xi - x) - theta
     k = np.argmin(np.abs(np.subtract(phi, z_pointing_angles)))
-    if r > np.min(Z_MAX, z_ranges[k] + ALPHA/2) or phi - z_pointing_angles[k] > BETA/2:
+    if r > np.min([Z_MAX, z_ranges[k] + ALPHA/2]) or np.abs(phi - z_pointing_angles[k]) > BETA/2:
         return L0
-    elif z_ranges[k] < Z_MAX and np.abs(r-z_ranges[k]) < ALPHA/2:
+    elif z_ranges[k] < Z_MAX and np.abs(r - z_ranges[k]) < ALPHA/2:
         return L_OCC
     else:
         return L_FREE
+
+# Update the map at every time step
+for time_step in range(len(ranges)):
+    state_t = states[:, time_step]
+    ranges_t = ranges[:, time_step]
+    bearings_t = bearings[:, time_step]
+    for i in range(100):
+        for j in range(100):
+            grid_cell_log_odds = grid_map[i][j]
+            grid_map[i][j] = grid_cell_log_odds + inverse_range_sensor_model([i, j], state_t, ranges_t, bearings_t, pointing_angles) - L0
+
